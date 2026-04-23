fe2d25c8025dcbf202f18572f263afb78640e368
Alex1a67 <yarisunalfirdaus11@gmail.com>
2026-04-23 18:45:01 +0700

    Delete Hotel-group-1 directory

--- Hotel-group-1/assets/js/ui.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", () => {
-  const nav = document.querySelector(".nav-wrap");
-  if (window.scrollY > 50) {
-    nav.style.background = "rgba(6,15,31,0.95)";
-  } else {
-    nav.style.background = "rgba(6,15,31,0.85)";
-  }
-});
